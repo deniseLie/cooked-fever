badbd972cd6f7432804aa947efa94f58e5ef7a1b
denise <22denise.lie@gmail.com>
2025-04-12 18:08:58 +0800

    foodsource

---
+++ app/src/main/java/com/example/cooked_fever/food/FoodSource.java
@@ -0,0 +1,45 @@
+package com.example.cooked_fever.food;
+
+import android.graphics.*;
+
+public class FoodSource {
+    private String foodSourceName;
+    private final Rect hitbox;
+    private float x, y;
+
+    private final Paint paint = new Paint();
+    private final Paint text = new Paint();
+
+    public FoodSource (int x, int y, String foodSourceName) {
+        hitbox = new Rect(x, y, x + 200, y + 200);
+        this.x = x;
+        this.y = y;
+        this.foodSourceName = foodSourceName;
+    }
+
+    // Getter
+    public String getFoodSourceName () {return foodSourceName;}
+    public float getX() {
+        return x;
+    }
+    public float getY() {
+        return y;
+    }
+
+    public void draw(Canvas canvas) {
+        Paint paint = new Paint();
+        paint.setColor(Color.GREEN);
+        canvas.drawCircle(x, y, 50, paint);
+
+        Paint text = new Paint();
+        text.setColor(Color.BLACK);
+        text.setTextSize(32f);
+        text.setAntiAlias(true);
+
+        canvas.drawText(this.foodSourceName, x - 60, y + 80, text);
+    }
+
+    public boolean isTouched(float touchX, float touchY) {
+        return hitbox.contains((int) touchX, (int) touchY);
+    }
+}
---
+++ app/src/main/java/com/example/cooked_fever/food/FoodSourceManager.java
@@ -0,0 +1,35 @@
+package com.example.cooked_fever.food;
+
+import android.graphics.*;
+import java.util.*;
+public class FoodSourceManager {
+
+    private final List<FoodSource> sources = new ArrayList<>();
+
+    public FoodSourceManager(int screenWidth, int screenHeight) {
+        addFoodSource(950, screenHeight - 650, "Burger");
+        addFoodSource(1900, screenHeight - 650, "Patty");
+    }
+
+    public void addFoodSource(int x, int y, String name) {
+        sources.add(new FoodSource(x, y, name));
+    }
+    public void draw(Canvas canvas) {
+        for (FoodSource source : sources) {
+            source.draw(canvas);
+        }
+    }
+
+    public FoodSource getTouchedSource(float touchX, float touchY) {
+        for (FoodSource source : sources) {
+            if (source.isTouched(touchX, touchY)) {
+                return source;
+            }
+        }
+        return null;
+    }
+
+    public List<FoodSource> getSources() {
+        return sources;
+    }
+}
