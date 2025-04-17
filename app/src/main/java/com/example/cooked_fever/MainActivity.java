8b528f753e835c1bb79e48dc6fb781e52794baf8
kohhuilyn <kohhuilyn@gmail.com>
2025-04-17 15:48:46 +0800

    add some sizzle and place item sounds

--- app/src/main/java/com/example/cooked_fever/MainActivity.java
+++ app/src/main/java/com/example/cooked_fever/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        com.example.cooked_fever.food.FoodItem.initSounds(this);
         Log.d("MainActivity", "onCreate called");
 
         gameView = findViewById(R.id.game_view);
